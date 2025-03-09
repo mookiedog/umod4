2c748406b7aa9c2d6ed494217e5dfb2c45cca413
Mookiedog <robin_hodgson@hotmail.com>
2025-03-09 15:15:27 -0700

    Updated WP board file for 4V1a PCB

--- WP/src/umod4_WP.h
+++ WP/src/umod4_WP.h
@@ -14,54 +14,65 @@
 
 #include "hardware/uart.h"
 
+// The GPIO pin assignments are as per the PCB 4V1 circuit board.
 
 // Now we define the extra stuff on the umod4 board that the PicoW will be driving:
-// Pin numbers refer to GPIO#
+// "Pin" numbers actually refer to GPIO ID, not a package pin number!
 
 // GPS connections:
-#define GPS_UART_ID       uart0
-#define GPS_TX_PIN        0       // We transmit to the GPS on this pin
-#define GPS_RX_PIN        1       // We receive from the GPS on this pin
-#define GPS_PPS_PIN       28
+#define GPS_UART_ID       uart1
+#define GPS_TX_PIN        8       // We transmit to the GPS on this pin
+#define GPS_RX_PIN        9       // We receive from the GPS on this pin
+#define GPS_PPS_PIN       7
 
 // WP can drive the EP's SWD port
 #define EP_SWCLK_PIN      2
 #define EP_SWDAT_PIN      3
 
 // This is the bidirectional link between the WP and EP
-#define BDLINK_UART_ID    uart1
-#define BDLINK_TX_PIN     4       // WP transmits on this pin
-#define BDLINK_RX_PIN     5       // WP receives on this pin
-
-#define BTN_PIN           26      // WP general purpose tacswitch
+#define BDLINK_UART_ID    uart0
+#define BDLINK_TX_PIN     0       // WP transmits on this pin
+#define BDLINK_RX_PIN     1       // WP receives on this pin
 
 // Spare IOs for future use
-#define SPARE1_PIN        9
-#define SPARE2_PIN        8
+#define SPARE0_PIN        27
+#define SPARE1_PIN        26
+#define SPARE2_PIN        5
 
 // Interface for driving a local LCD using RP2040 SPI1
-#define LCD_SPI_PORT      spi1
-#define LCD_BKLT_PIN      6
-#define LCD_DC_PIN        7
-#define LCD_SCK_PIN       10
-#define LCD_MOSI_PIN      11
-#define LCD_MISO_PIN      12
-#define LCD_CS_PIN        13
+#define LCD_SPI_PORT      spi0
+#define LCD_BKLT_PIN      21
+#define LCD_DC_PIN        20
+#define LCD_SCK_PIN       18
+#define LCD_MOSI_PIN      19
+#define LCD_MISO_PIN      16
+#define LCD_CS_PIN        17
 
 // Interface to MicroSD card using RP2040 SPI0
-#define SD_SPI_PORT       spi0
-#define SD_SCK_PIN        18
-#define SD_MOSI_PIN       19
-#define SD_MISO_PIN       16
-#define SD_CS_PIN         17
-#define SD_CARD_PIN       20
+#define SD_SPI_PORT       spi1
+#define SD_SCK_PIN        10
+#define SD_MOSI_PIN       11
+#define SD_MISO_PIN       12
+#define SD_CS_PIN         15
+#define SD_CARD_PIN       6
+
+// Alternate names for data GPIOs in 4-bit mode.
+// Must be 4 consecutively increasing GPIO numbers starting with SD_MOSI_PIN
+#define SD_DAT0           12
+#define SD_DAT1           13
+#define SD_DAT2           14
+#define SD_DAT3           15
 
 // The DataIn pin for driving a WS2812
 #define WS2812_PIN        22
 
-// Controls the EP 'RUN' (reset) signal. Active low to reset the EP.
-#define EP_RUN_PIN        27
+// Controls the EP 'RUN' (A.K.A. "!Reset") signal. Active low to reset the EP.
+#define EP_RUN_PIN        4
 
+// The WP retains the hardware ability to reset the ECU, but this feature is not used.
+// The reason is that we would not want a malfunctioning WP from preventing the ECU
+// from letting the engine run. We always want to be able to ride home!
+#define RESET_HC11        28
 
 // This might not be the right place, but it'll do for now:
 #define TASK_NORMAL_PRIORITY  ((tskIDLE_PRIORITY)+1)
